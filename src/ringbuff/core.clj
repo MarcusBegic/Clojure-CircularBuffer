c0e8eff202a214c09029e95015931886fccc59a1
marcusbegic <marcus.begic@gmail.com>
2020-11-18 13:27:45 +0100

    finisehd take and add

--- src/ringbuff/core.clj
+++ src/ringbuff/core.clj
@@ -1,23 +1,94 @@
+
 (ns ringbuff.core
   (:gen-class)
-(:import java.util.ArrayList)
+  (:require [clojure.core
+             :as core
+             :refer aset])
+  (:import java.util.ArrayList)
   )
 
-(deftype RingBuffer [n]
-  (let [^{:volatile-mutable true} start 0  ^{:volatile-mutable true} end 0
-        ^{:volatile-mutable true} capacity (- 1 (.size buf))
-        ^ArrayList buf (.ArrayList n)]
-  (pop
-    (let [result (.get buf start)]
-      (set! start (+ 1 start))
-      result)
-    )
+(defprotocol Buffer
+  (take! [buf] "return next item from buffer, called under chan mutex")
+  (adda [buf i])
+  )
 
-  (insert []
+(deftype ringbuffer [size
+                     ^{:volatile-mutable true} n
+                     ^{:volatile-mutable true} start
+                     ^{:volatile-mutable true} end
+                     ^{:volatile-mutable true} ^ArrayList buf
+                     ^{:volatile-mutable true} capacity]
+  Buffer
+  (take!
+    [this]
+      (if (> start n)
+          (let [return-val (aget buf 0)]
+            (set! start 0)
+            (aset buf start nil)
+            (set! start (inc start))
+            (set! capacity (inc capacity))
+            return-val)
+          (let [return-val (aget buf start)]
+            (set! start (inc start))
+            (set! capacity (inc capacity))
+            (return-val)
+            )
+         )
+      )
+
+  (adda
+    [this e]
+    (let [wrapper-index (fn [x] (let [m (mod x n)]
+                          (if (< m 0) (+ m n) m)))]
+      (if (and (not= capacity 0) (not= start (inc end)))
+        (if (> end n)
+            (do
+              (set! end 0)
+              (aset buf end e)
+              (set! end (inc end))
+              (set! capacity (dec capacity)))
+            (do
+              (aset buf end e)
+              (set! end (inc end))
+              (set! capacity (dec capacity)))
+              )
+        (do
+          (let [new-buf (java.util.ArrayList. (range (* 2 n)))]
+            (doseq [i (+ start (range n))]
+              (aset new-buf (- i start) (aget buf (wrapper-index i))))
+            (aset new-buf n e)
+            (set! buf new-buf)
+            )
+          (set! start 0)
+          (set! end n)
+          (set! capacity (dec n))
+          (set! n (* 2 n))
+              )
+            )
           )
+        )
+      )
 
+(defn make-ringbuffer [size]
+  (let [^{:volatile-mutable true} n size
+        ^{:volatile-mutable true} start 0
+        ^{:volatile-mutable true} end 0
+        ^{:volatile-mutable true} ^ArrayList buf (java.util.ArrayList. (range n))
+        ^{:volatile-mutable true} capacity (count buf)]
+    (ringbuffer. size n start end buf capacity))
   )
-)
+
+(defn make-arrau [n]
+  (java.util.ArrayList. (range n))
+  )
 
 (defn -main []
+  (let [buf (make-ringbuffer 10)]
+    (doseq [i (range 4)]
+      (adda buf i)
+      )
+    (doseq [i (range 4)]
+      (println (take! buf))
+      )
   )
+)

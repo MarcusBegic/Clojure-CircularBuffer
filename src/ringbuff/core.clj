317ff29d5493c8d2e450cdfd3b492d017017fc47
marcusbegic <marcus.begic@gmail.com>
2020-11-19 10:50:35 +0100

    volatile implementation of cicular

--- src/ringbuff/core.clj
+++ src/ringbuff/core.clj
@@ -18,56 +18,52 @@
   Buffer
   (take!
     [this]
-      (if (> start n)
-          (let [return-val (.get buf 0)]
-            (set! start 0)
-            (.add buf start nil)
-            (set! start (inc start))
-            (set! capacity (inc capacity))
+      (if (> @start @n)
+          (let [return-val (.get @buf 0)]
+            (vreset! start 0)
+            (.add @buf @start nil)
+            (vswap! start inc)
+            (vswap! capacity inc)
             return-val)
-          (let [return-val (.get buf start)]
-            (set! start (inc start))
-            (set! capacity (inc capacity))
-            (return-val)
+          (let [return-val (.get @buf @start)]
+            (vswap! start inc)
+            (vswap! capacity inc)
+            return-val)
             )
          )
-      )
 
   (adda
     [this e]
-    (let [wrapper-index (fn [x] (let [m (mod x n)]
-                          (if (< m 0) (+ m n) m)))]
-      (if (and (not= capacity 0) (not= start (inc end)))
-        (if (> end n)
+    (let [wrapper-index (fn [x] (let [m (mod x @n)]
+                          (if (< m 0) (+ m @n) m)))]
+      (if (and (not= @capacity 0) (not= @start (inc @end)))
+        (if (> @end @n)
             (do
-              (set! end 0)
-              (.add buf end e)
-              (set! end (inc end))
-              (set! capacity (dec capacity)))
-            (do
-              (.add buf end e)
-              (set! end (inc end))
-              (set! capacity (dec capacity)))
+              (vreset! end 0)
+              (.add @buf @end e)
+              (vswap! end inc)
+              (vswap! capacity dec)
               )
+            (do
+              (.add @buf @end e)
+              (vswap! end inc )
+              (vswap! capacity dec )
+              ))
         (do
-          (let [new-buf (java.util.ArrayList. (range (* 2 n)))]
-            (doseq [i (+ start (range n))]
-              (.add new-buf (- i start) (.get buf (wrapper-index i))))
-            (.add new-buf n e)
-            (set! buf new-buf)
+          (let [new-buf (java.util.ArrayList. (range (* 2 @n)))]
+            (doseq [i (range @n)]
+              (.add new-buf i (.get @buf (wrapper-index (+ @start i)))))
+            (.add new-buf @n e)
+            (vreset! buf new-buf)
             )
-          (set! start 0)
-          (set! end n)
-          (set! capacity (dec n))
-          (set! n (* 2 n))
+          (vreset! start 0)
+          (vreset! end @n)
+          (vswap! capacity dec)
+          (vswap! n * 2))
               )
             )
           )
         )
-      )
-
-
-
 
 ;; (defn make-ringbuffer [size]
 ;;   (let [ n size
@@ -80,24 +76,31 @@
 
 
 (defn make-ringbuffer [size]
-  (let [^{:volatile-mutable true} n size
-        ^{:volatile-mutable true} start 0
-        ^{:volatile-mutable true} end 0
-        ^{:volatile-mutable true} buf (java.util.ArrayList. (range n))
-        ^{:volatile-mutable true} capacity n]
+  (let [^{:volatile-mutable true} n (volatile! size)
+        ^{:volatile-mutable true} start (volatile! 0)
+        ^{:volatile-mutable true} end (volatile! 0)
+        ^{:volatile-mutable true} buf (volatile! (java.util.ArrayList. (range n)))
+        ^{:volatile-mutable true} capacity (volatile! size)]
     (ringbuffer. size n start end buf capacity)
-   ) 
+   )
+  )
+
+
+(defn printboi [^{:volatile-mutable true} capacity]
+  (vswap! capacity * 2)
   )
 
 (defn king [n]
-  (let [ ^{:volatile-mutable true} capacity (atom n)
-        ^{:volatile-mutable true} alist (java.util.ArrayList. (range n))]
-    (.add alist 3 22222222)
-    (swap! capacity inc )
-    capacity
-    )
+  (let [ ^{:volatile-mutable true} capacity  (volatile! n)
+        ^{:volatile-mutable true} alist (volatile! (java.util.ArrayList. (range n)))]
+    (.add @alist 3 22222222)
+    ;; (let [newlist (java.util.ArrayList. (range (* 2 n)))]
+    ;;   (vreset! alist newlist)
+    ;;   alist
+    ;; )
+    (printboi capacity)
   )
-  
+)
 
 (defn -main []
   (let [buf (make-ringbuffer 10)]
